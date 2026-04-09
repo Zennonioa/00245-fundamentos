eb79cf35649493a1bff9d875142f11287cff54dc
Marien <pintxo341@gmail.com>
2026-04-09 22:08:28 +0100

    2 commit aula01

--- fundamentos/Program.cs
+++ fundamentos/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Fundamentos01;
+
+
+public class Progam
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Inicio da Aula01 \n");
+        Aula01 aula01 = new Aula01();
+        aula01.ExecutarAula();
+
+
+
+
+
+
+    }
+    
+}//fim da class
--- fundamentos/obj/Debug/net10.0/fundamentos.AssemblyInfo.cs
+++ fundamentos/obj/Debug/net10.0/fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085a516631100a2edb128f7e16473a219f72edc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34e3372d6de7e3404a93f4803aec950ee13777b")]
 [assembly: System.Reflection.AssemblyProductAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
